d2af93145876984e51b4db90e27bd5e4e994886c
wesley <wesley.kargery@escolar.ifrn.edu.br>
2025-08-03 16:44:13 -0300

    Delete jogo directory

--- jogo/jogo/carro.py
+++
@@ -1,6 +0,0 @@
-import pygame
-class Carro:
-  def som_carro():
-    pygame.mixer.music.load('sounds/SOM DE CARRO PASSANDO(MP3_160K).mp3')
-    pygame.mixer.music.set_volume(5)
-    pygame.mixer.music.play(1)
